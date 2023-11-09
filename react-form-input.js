dd17facf9dffc4c1797b7f644f1dcaa2a3d0c27c
Brianoksoftware <79844711+Brianoksoftware@users.noreply.github.com>
2023-11-09 23:10:16 +0300

    Update and rename react-form-input to react-form-input.js
    Using useState, useEffect and forms, event handlers, input handling, submissions etc

--- react-form-input.js
+++ react-form-input.js
@@ -33,3 +33,42 @@ function App() {
 }
 
 export default App;
+
+
+//Example 2
+import React, {useState, useEffect} from 'react';
+import styles from './styles.css';
+
+function App(){
+  const[counter, setCounter] = useState(0);
+  const[input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log("input:", input);
+
+    return () => {
+      console.log("I'm a cleanup function");
+    }
+    
+  }, [input]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput(input);
+    //console.log("input:", input);
+  }
+  return(
+    <div className="App">
+      <h1 className="first-h1">{input}</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Enter fruit &nbsp;</label>
+        <input type ="text" value ={input} onChange={(e) => {setInput(e.target.value)}} /> <br /> <br />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default App;
